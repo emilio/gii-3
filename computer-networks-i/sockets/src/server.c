4d412a0c3d08cc94b0f1bbbf77fc4466c3d097ce
Emilio Cobos Álvarez <ecoal95@gmail.com>
2015-11-23 17:03:23 +0100

    server: Prototype server
    The UDP server should be working and able to receive packages. The TCP
    server should be able to receive packages after using `accept()`.
    
    I could try to reuse a bit of logic between both servers.

--- computer-networks-i/sockets/src/server.c
+++ computer-networks-i/sockets/src/server.c
@@ -9,9 +9,20 @@
 #include <stdbool.h>
 #include <string.h>
 #include <errno.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <unistd.h>
 
 #include "logger.h"
 #include "utils.h"
+
+#define TCP_MAX_CONNECTIONS 1024
+#define UDP_MAX_CONNECTIONS 1024
+#define MAX_MESSAGE_SIZE 512
+
 
 struct program_author {
     const char* name;
@@ -40,14 +51,111 @@ void show_usage(int argc, char** argv) {
     }
 }
 
-int run_server(long port) {
-    // TODO
-    return 0;
+void* start_tcp_server(void* info) {
+    long port = *((long*) info);
+    struct sockaddr_in serv_addr;
+    int sock;
+    int ret;
+
+    LOG("start_tcp_server(%p); port: %ld", info, port);
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        ERROR("Couldn't open TCP socket: %s", strerror(errno));
+        return NULL;
+    }
+
+    LOG("Opened TCP socket (fd: %d)", sock);
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    ret = bind(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
+    if (ret == -1) {
+        ERROR("Couldn't bind TCP socket: %s", strerror(errno));
+        goto cleanup_and_return;
+    }
+
+    LOG("TCP socket bound to port: %ld", port);
+
+    ret = listen(sock, TCP_MAX_CONNECTIONS);
+    if (ret == -1) {
+        ERROR("Listen: %s", strerror(errno));
+        goto cleanup_and_return;
+    }
+
+cleanup_and_return:
+    LOG("TCP server closing");
+    close(sock);
+    return NULL;
+}
+
+void* start_udp_server(void* info) {
+    long port = *((long*) info);
+    struct sockaddr_in serv_addr;
+    int sock;
+    int ret;
+
+    LOG("start_udp_server(%p); port: %ld", info, port);
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        ERROR("Couldn't open UDP socket: %s", strerror(errno));
+        return NULL;
+    }
+
+    LOG("Opened UDP socket (fd: %d)", sock);
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    ret = bind(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
+    if (ret == -1) {
+        ERROR("Couldn't bind UDP socket: %s", strerror(errno));
+        close(sock);
+        return NULL;
+    }
+
+    LOG("UDP socket bound to port: %ld", port);
+
+    while (true) {
+        struct sockaddr_in src_addr;
+        socklen_t src_addr_len = sizeof(src_addr);
+        ssize_t len;
+        char buff[MAX_MESSAGE_SIZE];
+
+        len = recvfrom(sock, buff, sizeof(buff), 0, (struct sockaddr*) &src_addr, &src_addr_len);
+        if (len == -1) {
+            WARN("Ignoring UDP datagram due to error: %s", strerror(errno));
+            continue;
+        }
+
+        LOG("udp: Received %zu bytes: \"%s\"", len, buff);
+
+        len = sendto(sock, buff, len, 0, (struct sockaddr*) &src_addr, src_addr_len);
+
+        if (len == -1) {
+            WARN("UDP response lost: %s", strerror(errno));
+            continue;
+        }
+    }
+
+    close(sock);
+    return NULL;
 }
 
 int main(int argc, char** argv) {
     int i;
     long port = 8000;
+    int thread_creation_status;
+
+    pthread_t tcp_thread;
+    pthread_t udp_thread;
+
     LOGGER_CONFIG.log_file = stderr;
 
     for (i = 0; i < argc; ++i) {
@@ -69,5 +177,22 @@ int main(int argc, char** argv) {
 
     LOG("Starting server on port %ld", port);
 
-    return run_server(port);
+    /// NOTE: Passing a pointer to a variable on the stack is unsafe if `main()` does not
+    /// live long enough. It does though, so...
+    thread_creation_status = pthread_create(&tcp_thread, NULL, start_tcp_server, &port);
+    if (thread_creation_status != 0)
+        FATAL("Couldn't create TCP server thread: %s", strerror(thread_creation_status));
+
+    LOG("TCP server thread created: %ld", tcp_thread);
+
+    thread_creation_status = pthread_create(&udp_thread, NULL, start_udp_server, &port);
+    if (thread_creation_status != 0)
+        FATAL("Couldn't create UDP server thread: %s", strerror(thread_creation_status));
+
+    LOG("UDP server thread created: %ld", udp_thread);
+
+    pthread_join(tcp_thread, NULL);
+    pthread_join(udp_thread, NULL);
+
+    return 0;
 }

6a5db7f0a207898f87e6b3fc88910cfcbe4f3b71
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-05-01 18:17:30 +0200

    cgi: Add wip status page.

---
+++ sysadmin/final/app/modules/cgi/cgi/status.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+use Proc::ProcessTable;
+use CGI::Template;
+
+my @SERVICES = (
+  "apache2", # This is kind of stupid, the page won't load if apache isn't running.
+  "sshd",
+  "ftpd",
+  "postfix",
+  "saslauthd",
+  "sysadmin-appd",
+);
+
+my $table = new Proc::ProcessTable;
+
+my $status = "";
+foreach $service (@SERVICES) {
+  my $service_status = "not-running";
+  foreach $process (@{$table->table}) {
+    if ($process->cmndline ~= /$service/) {
+      $service_status = "running";
+      break;
+    }
+  }
+
+  $status .= "<li class=\"$service status-$service_status\">";
+  $status .= "$service <span class=\"status\"></span>";
+  $status .= "</li>";
+}
+
+my $template = new CGI::Template;
+print $template->header();
+print $template->content(
+  STATUS => $status,
+);

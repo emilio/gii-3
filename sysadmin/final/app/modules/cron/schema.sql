197e8b84bbb389f70aea469fafa6bb078fc7962a
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-05-01 17:08:44 +0200

    sysadmin: Take rid of modules/cron
    It's superseeded by modules/daemon

--- sysadmin/final/app/modules/cron/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE user_requests (
-  username VARCHAR(255) NOT NULL,
-  plain_password VARCHAR(255) NOT NULL,
-  PRIMARY KEY (username)
-);
--- sysadmin/final/app/modules/cron/setup.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE app;
-DROP USER app;
-
-/* TODO Don't hardcode this */
-CREATE USER app
-  WITH PASSWORD '12345678';
-
-CREATE DATABASE app OWNER app;

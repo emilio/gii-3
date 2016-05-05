ca4ff4beb6990902dcb662d05a544e4064eaeaa7
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-05-06 00:57:38 +0200

    fuck windlx man... Actually got to work a matrix inversion
    Wine + random windlx crashes + can't run it on win64... sigh

--- computer-architecture/dlx/dumb.s
+++ computer-architecture/dlx/dumb.s
@@ -1,8 +1,8 @@
-.data 0x1000
+.data
 ; Input matrix 1
 M1:
-  .float 5.234, 2.52
-  .float 1.23,  7.52
+  .float 1.0, 0.0
+  .float 0.0, 1.0
 
 ; Input matrix 2
 M2:
@@ -16,25 +16,46 @@ MR:
   .float 0, 0, 0, 0
   .float 0, 0, 0, 0
 
+ONE:
+  .float 1.0
+
 .text
 .global main
 
 main:
-  lw r1, M1(0)
-  jal determinant
+  add r1, r0, M1
+  add r2, r0, M2
 
-  halt
+  jal inverse
 
-; Computes the determinant of a matrix and stores the result in f0
-; Gets the data from the register 1's address
-determinant:
-  ; Get the four floats into the registers f0..3
+  trap 0
+
+; Computes the inverse of a matrix of address r1,
+; and leaves the result in r2
+inverse:
   lf f0, 0(r1)
-  lf f1, 1(r1)
-  lf f2, 2(r1)
-  lf f3, 3(r1)
-  mult f0, f0, f3 ; First diagonal in f4
-  mult f1, f1, f2 ; Second in f5
-  sub f0, f1, f0
+  lf f1, 4(r1)
+  lf f2, 8(r1)
+  lf f3, 12(r1)
+
+  ; First diagonal in f4, second in f5
+  multf f4, f0, f3
+  multf f5, f1, f2
+  subf f4, f4, f5
+
+  ; f4 -> determinant
+  ; f5 -> 1 / determinant
+  lf f6, ONE
+  divf f5, f6, f4
+
+  multf f0, f0, f5
+  multf f1, f1, f5
+  multf f2, f2, f5
+  multf f3, f3, f5
+
+  sf 0(r2), f0
+  sf 4(r2), f1
+  sf 8(r2), f2
+  sf 12(r2), f3
 
   jr r31

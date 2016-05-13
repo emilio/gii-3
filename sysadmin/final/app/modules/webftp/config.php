0e6c1176144770ea27863c0e075efad0793467fc
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-05-13 12:15:10 +0200

    Add webftp service
    Because why not?

---
+++ sysadmin/final/app/modules/webftp/config.php
@@ -0,0 +1,22 @@
+<?php
+
+# Open README file for descriptions and help.
+
+$ftpHost = "127.0.0.1";
+$ftpPort = "21";
+$ftpMode = "1";
+$ftpSSL  = "1";
+$ftpDir  = "";
+$serverTmp = "";
+$editableExts = "asp,ashx,asmx,aspx,asx,axd,cfm,cgi,css,html,htm,jhtml,js,php,phtml,pl,txt,xhtml";
+$sessionName = "";
+$dateFormatUsa = 0;
+$lockOutTime = 5;
+$versionCheck = 1;
+$showAdvOption = 1;
+$showLockSess = 1;
+$showHostInfo = 1;
+$showAddons = 1;
+$showDotFiles = 1;
+
+?>

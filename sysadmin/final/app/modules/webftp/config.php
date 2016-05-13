0385e4afacc50b8387b916ea741a31fc51475ae1
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-05-13 12:49:55 +0200

    ftp: Make mftp work

--- sysadmin/final/app/modules/webftp/config.php
+++ sysadmin/final/app/modules/webftp/config.php
@@ -5,9 +5,12 @@
 $ftpHost = "127.0.0.1";
 $ftpPort = "21";
 $ftpMode = "1";
-$ftpSSL  = "1";
+// $ftpSSL  = "1";
+// TODO: enable ssl? If we're using webftp over https, given the
+// connection is local, shouldn't be necessary.
+$ftpSSL  = "0";
 $ftpDir  = "";
-$serverTmp = "";
+// $serverTmp = "";
 $editableExts = "asp,ashx,asmx,aspx,asx,axd,cfm,cgi,css,html,htm,jhtml,js,php,phtml,pl,txt,xhtml";
 $sessionName = "";
 $dateFormatUsa = 0;
